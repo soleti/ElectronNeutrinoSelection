c17ee6354f90c0e8cc1778fbb93d4b3080d2bc5b
Travis CI <travis@travis-ci.org>
2018-08-14 23:20:00 +0000

    Deploy code docs to GitHub Pages Travis build: 38
    Commit: 91a496d25c80b069acacb25be7496da78aa9b303

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -5,5 +5,6 @@ var searchData=
   ['choose_5fcandidate',['choose_candidate',['../group__lee.html#ga2598c705e4307eed73e1989613028ced',1,'lee::PandoraLEEAnalyzer']]],
   ['clear',['clear',['../group__lee.html#gad936812d5e9dc7999a37c75b84b04ada',1,'lee::ElectronEventSelectionAlg::clear()'],['../group__lee.html#ga9d8072bae8c7e78701c093070d17c992',1,'lee::PandoraLEEAnalyzer::clear()']]],
   ['cluster_5fresiduals',['cluster_residuals',['../group__lee.html#ga01205d4a5d2f8d5ad0d4d5462e0b7563',1,'lee::EnergyHelper']]],
-  ['collect3dhits',['collect3DHits',['../group__lee.html#ga06a3e470d9b2ca729d25822fac19e455',1,'lee::ElectronEventSelectionAlg']]]
+  ['collect3dhits',['collect3DHits',['../group__lee.html#ga06a3e470d9b2ca729d25822fac19e455',1,'lee::ElectronEventSelectionAlg']]],
+  ['collectmcparticles',['CollectMCParticles',['../group__lee.html#ga3a9ecae08d8a56e37cc9ef723eca6907',1,'lee::PandoraInterfaceHelper::CollectMCParticles(const art::Event &amp;evt, const std::string &amp;label, lar_pandora::MCParticleVector &amp;particleVector)'],['../group__lee.html#ga2c7126fa2d3a506830f25ccad4fa0b3c',1,'lee::PandoraInterfaceHelper::CollectMCParticles(const art::Event &amp;evt, const std::string &amp;label, lar_pandora::MCTruthToMCParticles &amp;truthToParticles, lar_pandora::MCParticlesToMCTruth &amp;particlesToTruth)']]]
 ];
